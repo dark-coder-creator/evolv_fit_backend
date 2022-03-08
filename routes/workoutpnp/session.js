21c16afd259c6ec15558b8fa081308cbe98c89d5
Cheloor Saravanan Mohandas <saravanan.mohandas24@gmail.com>
2022-03-08 22:42:11 +0530

    update date task completed

--- routes/workoutpnp/session.js
+++ routes/workoutpnp/session.js
@@ -21,6 +21,13 @@ const Workout = mongoose.model('Workout',workoutSchema)
   
 //  }
 
+// {
+//     "workout_id":"62277ed7925ec74114df45ae",
+//     "date":"2022-03-10",
+//    "userRef":"6221cf7d67e1c8d243417e98",
+//    "trainerRef":"6221cef9b1b67592b349b55e",
+//    "isCompleted":false
+//  }
 router.post('/',(req,res) => {
   
 
@@ -57,6 +64,57 @@ router.post('/',(req,res) => {
     
 })
 
+// {
+//     "sessionId":"62277fbd17df5d083c6a24ea"
+//  }
+router.put('/update_date',(req,res) =>{
+    const { sessionId } =req.body 
+    Session.findById(sessionId,(err,docs) => {
+        if(err)
+        {
+            throw err
+        }
+        else 
+        {
+           
+            const date = docs.date
+            if(docs.isCompleted === false)
+            {
+                // let text = date.toString()
+                
+                
+                // let splittedDate = text.split(" ") 
+                // let number = parseInt(splittedDate[2])
+                // let addedDate = number+1;
+                //  var stringDate = addedDate.toString()
+                //  splittedDate[0] = 'Fri'
+                // splittedDate[2] = stringDate;
+                // console.log(splittedDate)
+                //  var modifiedDate = splittedDate.join()
 
+                // var increasedDate = new Date(modifiedDate)
+                const addedDate =date.getDate()+1;
+                date.setDate(addedDate)
+
+                Session.findByIdAndUpdate(sessionId,{date:date},(err,docs) =>{
+                    if(err)
+                    {
+                        throw error
+                    }
+                    else 
+                    {
+                        res.status(200).json(docs)
+                    }
+                })
+               
+            }
+            else 
+            {
+                res.status(404).json(docs)
+            }
+        }
+    
+    })
+})
 
 module.exports = router
--- routes/workoutpnp/workout.js
+++ routes/workoutpnp/workout.js
@@ -8,10 +8,13 @@ const workoutSchema = require('../../models/WorkoutPnP/workout')
 const Workout = mongoose.model('Workout',workoutSchema)
 
 
-var router = express.Router();
-router.use(bodyParser.json()) 
+const exerciseSchema = require('../../models/WorkoutPnP/exercise')
+const Exercise = mongoose.model('Exercise',exerciseSchema)
 
-router.get('/',(req,res) => {
+var workoutRouter = express.Router();
+workoutRouter.use(bodyParser.json()) 
+
+workoutRouter.get('/',(req,res) => {
 
     Workout.find({},(err,docs) => {
         if(err)
@@ -28,8 +31,10 @@ router.get('/',(req,res) => {
    
 })
 
-router.put('/add_workout_data',(req,res) => {
+workoutRouter.put('/add_workout_data',(req,res) => {
     const obj = {"name":"Squat","exerciseInfoRef":"6221f77d9779d825dfc4ad0e","exerciseSets":{"number":4,"suggestedWeight":82,"suggestedReps":12}}
+
+
 
     Workout.findOneAndUpdate({workout_id:req.body.workout_id},{ $push:{ exercises:obj}},function(err,result) {
         if(err) {
@@ -45,4 +50,48 @@ router.put('/add_workout_data',(req,res) => {
 
 
 
-module.exports = router
+// {
+//     "workoutName":"Evening Workout",
+//     "exerciseId":"6225fce6ef910e5aae6620b9"
+// }
+workoutRouter.post('/workout_data',(req,res) => {
+    const {workoutName,exerciseId} = req.body 
+
+   
+    Exercise.findById(exerciseId,(err,docsOne) => {
+            if(err)
+            {
+                throw err
+            }
+            else 
+            {
+                 //res.status(200).json(docs)
+                 console.log(docsOne.exerciseInfoRef)
+                 const exerciseInfoRef = docsOne.exerciseInfoRef
+                 const exerciseSets = docsOne.exerciseSets
+                 const name = docsOne.name
+               const Workouts = new Workout({ name:workoutName , exercises:{exerciseInfoRef,name,exerciseSets}})
+               Workouts.save((err,result) => {
+                   if(err)
+                   {
+                       throw err
+                   }
+                   res.status(200).json(result)
+               })
+            }
+    }) 
+
+    // const Workouts = new Workout({ name , exercises })
+    // Workouts.save(function(err,result) {
+    //     if(err)
+    //     {
+    //         console.log(err)
+    //     }
+    //     else {
+    //         res.status(200).json(result);
+    //     }
+    // })
+})
+
+
+module.exports = workoutRouter
